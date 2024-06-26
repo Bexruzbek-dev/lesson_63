f4422adb6c0fcfad1303a7c0a318024ce2622cb4
Bexruzbek-dev <behruz.toshpolatov14@gmail.com>
2024-06-26 11:00:33 +0500

    first

---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+// import 'package:lesson_63/views/widgets/painter.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.blueAccent.shade100,
+//       body: Center(
+//         child: Container(
+//           color: Colors.white,
+//           child: CustomPaint(
+//             size: const Size(double.infinity, 700),
+//             painter: MyPainter(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
